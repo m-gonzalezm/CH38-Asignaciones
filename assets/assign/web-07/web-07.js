332fa7afd87f061104f8b87d31db5c970433a5bf
Miguel <Miguel_a.GM@hotmail.com>
2024-03-31 03:32:31 -0600

    feat: implemented recalling - greeting

--- assets/assign/web-07/web-07.js
+++ assets/assign/web-07/web-07.js
@@ -1,14 +1,16 @@
-function setName() {
-    const name = prompt('¿Cuál es tu nombre?');
-    if (name) {
-        setTextByID(`¡Hola, ${name}!`, "name-text");
-        setTextByID("¡Cambiar el nombre!", "button");
-    } else {
-        setTextByID('¡Hola!', "name-text");
-        setTextByID("¡Ingresa tu nombre!", "button");
-    }
-}
-
-function setTextByID(text, id) {
+const renderTextById = (text, id) => {
     document.getElementById(id).innerHTML = text;
 }
+
+const renderGreeting = greeting => {
+    const name = localStorage.getItem("name");
+    renderTextById(name ? `${greeting}, ${name}!` : "Hello!", "greeting");
+    renderTextById(name ? "Change the name!" : "Enter your name!", "button");
+};
+
+const getName = () => {
+    localStorage.setItem("name", prompt("What's your name?"));
+    renderGreeting("Hello");
+}
+
+renderGreeting("Welcome back");
